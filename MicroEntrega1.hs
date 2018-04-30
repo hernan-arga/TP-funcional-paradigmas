230523220de87ae8668bc0ac26758e138ccf1f2f
hernan-arga <37780035+hernan-arga@users.noreply.github.com>
2018-04-29 23:35:00 -0300

    Update MicroEntrega1.hs

--- MicroEntrega1.hs
+++ MicroEntrega1.hs
@@ -3,10 +3,15 @@ data Microprocesador = Microprocesador {memoriaDeDatos :: [Int], acumuladorA :: 
 xt8088 = Microprocesador {memoriaDeDatos=[], acumuladorA = 0, acumuladorB = 0, programCounter = 0, etiqueta = ""}
 
 --PUNTO 3.2.1
-nop = xt8088 {programCounter = programCounter xt8088 + 1} 
+nop unMicro = unMicro {programCounter = programCounter xt8088 + 1} 
 
 --PUNTO 3.2.2
 --avanzarTresVecesProgramCounter = 
 
 --PUNTO 3.3.1
-lodv val = xt8088 {acumuladorA = val}
+lodv val unMicro = unMicro {acumuladorA = val}
+
+swap = (modificarAcumuladorA.modificarAcumuladorB).nop
+
+modificarAcumuladorA unMicro = unMicro {acumuladorA = acumuladorB xt8088}
+modificarAcumuladorB unMicro = unMicro {acumuladorB = acumuladorA xt8088}
